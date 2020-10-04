6ce9eaa7ba365bf3f111ba1b8c67a815c613acd0
codegitpro <michael.jin.dev@gmail.com>
2020-10-04 23:03:09 +0900

    implement storage class based on file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,13 +30,46 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'My APP'),
+      home: MyHomePage(title: 'My APP', storage: CounterStorage()),
     );
   }
 }
 
+class CounterStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+
+      String contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      return 0;
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    return file.writeAsString('$counter');
+  }
+}
+
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  final CounterStorage storage;
+
+  MyHomePage({Key key, this.title, @required this.storage}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -43,16 +80,24 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readCounter().then((int value) {
+      setState(() {
+        _counter = value;
+      });
+    });
+  }
+
+  Future<File> _incrementCounter() async {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -61,6 +106,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+
+    return widget.storage.writeCounter(_counter);
   }
 
   @override
@@ -81,7 +128,7 @@ class _MyHomePageState extends State<MyHomePage> {
         itemCount: _counter,
         itemBuilder: (context, index) {
           return ListTile(
-            leading: Icon(Icons.account_circle),
+            leading: Icon(Icons.account_balance),
             title: Text('Name of item'),
             onTap: () {
               Navigator.push(
@@ -109,11 +156,12 @@ class SecondPage extends StatelessWidget {
         title: Text('Second Screen'),
       ),
       body: Center(
-        child: RaisedButton(
+        child: RaisedButton.icon(
           onPressed: () {
             Navigator.pop(context);
           },
-          child: Text('Go Back!'),
+          label: Text('Go back!'),
+          icon: Icon(Icons.access_alarm),
         ),
       ),
     );
